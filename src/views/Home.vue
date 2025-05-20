06b11add71ee0b78b4dbce184688ceea41f13499
Choo ChangWoo <cnckddn0146@gmail.com>
2025-05-20 16:34:33 +0900

    update main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,22 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="flex flex-col gap-4">
-      <router-link
-        to="/a"
-        class="block text-center px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600"
-      >
-        창우
-      </router-link>
-      <router-link
-        to="/b"
-        class="block text-center px-6 py-3 bg-orange-500 text-white rounded hover:bg-orange-600"
-      >
-        희찬
-      </router-link>
-      <router-link
-        to="/c"
-        class="block text-center px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600"
-      >
-        태민
-      </router-link>
-      <router-link
-        to="/d"
-        class="block text-center px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
-      >
-        소희
-      </router-link>
-    </div>
+  <div class="flex flex-col min-h-screen">
+    <header class="h-24 bg-sky-950 text-white text-4xl">
+      <div class="w-full h-full mx-auto flex justify-center items-center">가디의삶</div>
+    </header>
+
+    <main class="grow">
+      <!-- 각 페이지 컴포넌트 출력 위치 -->
+      <router-view />
+    </main>
+
+    <footer class="h-24 bg-blue-800"></footer>
   </div>
 </template>
+
+<script setup>
+const name = '홍길동'
+</script>
+
+<style scoped>
+/* main에 @apply는 필요 없음. 클래스에서 처리 */
+</style>
