7a76fdc6a4a10f5ae2886aa7eaf73a110f069f4b
Choo ChangWoo <cnckddn0146@gmail.com>
2025-05-20 15:19:48 +0900

    add Example Card

--- src/views/personal/d.vue
+++ src/views/personal/d.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="h-screen w-full flex justify-center items-center">
+  <div class="h-screen w-full flex flex-col justify-center items-center">
+    <BaseCard />
     <div class="text-5xl">Hi</div>
   </div>
 </template>
 
-<script scoped></script>
+<script scoped>
+import BaseCard from '@/components/BaseCard.vue'
+</script>
 
 <style scoped></style>
